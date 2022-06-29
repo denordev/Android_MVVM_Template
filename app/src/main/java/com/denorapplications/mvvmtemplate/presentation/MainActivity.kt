4f22fb7961099b6c3c7b3ed451829539bfc7708a
Denis Orlov <denisorlov.dev@gmail.com>
2022-06-29 11:14:27 +0300

    Add back button support

--- app/src/main/java/com/denorapplications/mvvmtemplate/presentation/MainActivity.kt
+++ app/src/main/java/com/denorapplications/mvvmtemplate/presentation/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.denorapplications.mvvmtemplate.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.denorapplications.mvvmtemplate.R
 import com.denorapplications.mvvmtemplate.appComponent
 import com.denorapplications.mvvmtemplate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val navHostFragment: NavHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+    }
+    private val navController: NavController by lazy { navHostFragment.navController }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         appComponent.inject(this)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupActionBarWithNavController(navController)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
